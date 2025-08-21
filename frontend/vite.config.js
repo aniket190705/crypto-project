6784dc37dd4d6e1c4cd676fcce12f1a71f8668d6
Amanpiro <ag7921676@gmail.com>
2025-08-21 20:55:32 +0530

    dekh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Enables Tailwind v4 with Vite
-  ],
-})
+  plugins: [react(), tailwindcss()],
+});
